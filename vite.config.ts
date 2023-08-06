efcc7c746ef6e73adca7613c09bba6b90f8fe9ff
samnsimson <samnishanth01@gmail.com>
2023-08-06 19:45:51 -0400

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const root = path.resolve(__dirname, 'src')
+const outDir = path.resolve(__dirname, 'dist')
+const publicDir = path.resolve(__dirname, 'public')
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    root,
+    publicDir,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: path.resolve(root, 'index.html'),
+            },
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),

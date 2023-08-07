1620fb69c4b132da2998fa7b72f232a0c23bab48
samnsimson <samnishanth01@gmail.com>
2023-08-07 00:58:49 -0400

    hash router

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ const outDir = path.resolve(__dirname, 'dist')
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    appType: 'spa',
     root,
     base: '/',
     server: {
